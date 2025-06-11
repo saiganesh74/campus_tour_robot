e5edbf8344f378a27ef6f5911fb961ffa08d68f8
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-11 20:26:48 +0530

    Added voice greeting

--- src/greeting/greeting/greeter.py
+++ src/greeting/greeting/greeter.py
@@ -2,12 +2,21 @@
 
 import rclpy
 from rclpy.node import Node
+import pyttsx3
 
 class CampusGreeter(Node):
     def __init__(self):
         super().__init__('campus_greeter')
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 150)
+        self.engine.setProperty('volume', 0.9)
         self.get_logger().info('👋 Welcome to the Campus Tour!')
+        self.speak('Welcome to the Campus Tour...')
         self.get_logger().info('🤖 I am your tour guide robot.')
+        self.speak('I am your tour guide robot....')
+    def speak(self , text):
+        self.engine.say(text)
+        self.engine.runAndWait()
 
 def main():
     rclpy.init()
